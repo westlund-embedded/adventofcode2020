d45f0b82a14174d803f2975a06444bb62b2a5b05
jw <johan@westlund-embedded.se>
2020-12-05 00:01:56 +0100

    have print string

--- calendar/day3/code-1.asm
+++ calendar/day3/code-1.asm
@@ -1,40 +1,39 @@
 section     .data
-msg db      "Test",0xa
+msg db      "Testing testing",0xa,0
 
 
 section     .text
 global      _start
 
 _start:                             ; Entry point
-    mov     eax, msg                ; sys_write
-    call    print
-    call    exit
-
-
+    push    rax
+    mov     rax, msg                ; put msg as argument to print
+    call    print                   ; call sub routine
+    pop     rax
+    call    exit                    ; Exit
+    
 print: 
-    mov     esi, eax
-    mov     eax, 1
-    mov     edi, 1
-    call    strlen
+    call    strlen                  ; get strlen -> edx
+    mov     rsi, rax                ; put msg in source reg
+    mov     rax, 1                  ; sys_write
+    mov     rdi, 1                  ; stdout
     syscall
     ret
 
 strlen:
-    push    rbx
-    mov     ebx, esi
-
-next_char:
-    cmp     byte[ebx], 0
-    inc     ebx
-    jnz     next_char
-    dec     ebx
-    sub     ebx, esi
-    mov     edx, ebx
-    pop     rbx
+    push    rcx
+    mov     rdi, rax                ; put str in dest reg
+    xor     al, al                  ; clread compare reg
+    xor     rcx, rcx                ; clear ecx
+    not     rcx                     ; -1 = search all
+    cld                             ; clear direction flag
+    repne   scasb                   ; search string until '\0'
+    sub     rdi, rax                ; calc length
+    mov     rdx, rdi                ; store result in edx    
+    pop     rcx
     ret
 
-
 exit: 
-    mov     eax, 60                 ; sys_exit
-    xor     edi, edi                ; return 0
+    mov     rax, 60                 ; sys_exit
+    xor     rdi, rdi                ; return 0
     syscall
